78f8d25d2e5de39ba6c06e6889ca8f0e4999167c
Achintya Sunny <achintyasunny74@gmail.com>
2026-08-30 15:36:29 +0530

    Add unique healthz endpoint

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from src.payment_server import app
 
+
+@app.route("/healthz")
+def healthz():
+    return {
+        "status": "ok",
+        "message": "Payment backend is running"
+    }
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
